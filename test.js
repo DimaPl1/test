ef38d29fdc3fedade5c900b471011b855b27a464
Dima <plisyuk.dmitry222@gmail.com>
2023-06-30 12:28:05 +0300

    commit

--- test.js
+++ test.js
@@ -29,4 +29,4 @@ function findUniqueChar(text) {
   const text =
     "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup";
   const result = findUniqueChar(text);
-  console.log(result); // Виведе: "m"
+  console.log(result);
